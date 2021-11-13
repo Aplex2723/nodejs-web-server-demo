1e4854fe7b4b366b01c0d2b96b18cb88a14fbb97
Uziel Torres <72839304+Aplex2723@users.noreply.github.com>
2021-11-13 11:07:41 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const hbs = require('hbs');
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
+//* Ruta principal
+app.use( express.static('public'))
+
+// app.get('/', function (req, res) {
+
+//   res.render('home', {
+//     titulo: 'Game Page',
+//     head: 'GAMEPAGE'
+//   })
+
+// })
+
+// //* Subrutas
+// app.get('/about', function (req, res) {
+
+//   res.render('about', {
+//     title: 'About Us',
+    
+//   })
+
+// })
+
+// app.get('/contact-me', function (req, res) {
+
+//   res.render('login')
+
+// })
+
+//*Ruta redireccional
+app.get('*', function (req, res) {
+
+    res.sendFile(__dirname + '/public/index.html')
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
